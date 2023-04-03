28a3d73853a3e2d9e8b1c6d024481b6ee0803386
Tianxing Wu <chi0chu319@gmail.com>
2023-04-03 20:55:36 +0800

    updated output doc

--- blueprints/match.py
+++ blueprints/match.py
@@ -51,6 +51,7 @@ def top_k_courses(query_text, k):
 
 @match_blueprint.get('/match/<course_code>')
 @match_blueprint.output(Course(many=True))
+@match_blueprint.output(status_code=400)
 def match_code(course_code):
     courses = get_saved_courses()
 
